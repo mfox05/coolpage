0dfbc45ebd70c4ad76c2f7c6cf9db75986509bf9
mfox05 <maciejfoks22@gmail.com>
2023-05-30 09:34:31 +0200

    Site done

--- js/main.js
+++ js/main.js
@@ -1,24 +1,45 @@
-const navBtn = document.querySelector('.burger-btn');
-const nav = document.querySelector('.nav');
+const navBtn = document.querySelector('.burger-btn')
+const nav = document.querySelector('.nav')
 const allNavItems = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
-const handleNav = () =>{
-    nav.classList.toggle('nav--active')
-    allNavItems.forEach( item => {
-        item.addEventListener('click', () => {
-            nav.classList.remove('nav--active')
-        })
-    })
-    handleNavItemsAnimation();
+const handleNav = () => {
+    navBtnBars.classList.remove('black-bars-color')
+	nav.classList.toggle('nav--active')
+	allNavItems.forEach(item => {
+		item.addEventListener('click', () => {
+			nav.classList.remove('nav--active')
+		})
+	})
+	handleNavItemsAnimation()
 }
 
 const handleNavItemsAnimation = () => {
-    let delayTime = 0;
-    allNavItems.forEach(item => {
-        item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + 's';
-        delayTime++;
+	let delayTime = 0
+	allNavItems.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+const handleObserver = () => {
+    const currentSection = window.scrollY;
+    allSections.forEach(section =>{
+        if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-color')
+        } else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-color')
+        }
     })
 }
 
+
+handleCurrentYear();
+window.addEventListener('scroll', handleObserver)
 navBtn.addEventListener('click', handleNav);
